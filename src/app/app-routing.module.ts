a945e53fda2e7537231579145fa81ef766ec0a69
patrira <iradukundapatrick19@gmail.com>
2024-09-05 22:53:40 +0200

    Handling Styling with tailwind css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductModule } from './components/product/product.module';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
